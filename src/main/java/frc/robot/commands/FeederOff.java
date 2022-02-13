388d322d99b092661a2563227d96bc4e43ef410c
LavaWaffle <60158215+LavaWaffle@users.noreply.github.com>
2022-02-13 16:04:16 -0500

    removed feederOff from end

--- src/main/java/frc/robot/commands/FeederOff.java
+++ src/main/java/frc/robot/commands/FeederOff.java
@@ -53,7 +53,6 @@ public class FeederOff extends CommandBase
     @Override
     public void end(boolean interrupted) 
     {
-        m_feeder.feederOff();
     }
 
     // Returns true when the command should end.
