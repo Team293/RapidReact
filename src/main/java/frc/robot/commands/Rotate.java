8dabded3e9a5c99d22a714a96e02c9cb6208335a
Karthicc <37190705+cartha353@users.noreply.github.com>
2022-04-02 06:51:02 -0400

    first attempt at pid based on navx

--- src/main/java/frc/robot/commands/Rotate.java
+++ src/main/java/frc/robot/commands/Rotate.java
@@ -9,11 +9,21 @@ public class Rotate extends CommandBase {
     private final Drivetrain m_drivetrain;
     private double m_targetDegrees;
     private double initalAngle;
+    private double m_lastError = 0.0d;
+    private double m_errorIntegral = 0.0d;
+    private double m_error;
+    private double m_change;
+    //Base values for tuning
+    private double m_vP = 0.01;
+    private double m_vI = 0.000;
+    private double m_vD = 0.1;
+
 
     public Rotate(Drivetrain drivetrain, double targetDegrees) {
         m_drivetrain = drivetrain;
         m_targetDegrees = targetDegrees;
         initalAngle = m_drivetrain.getGyroYawDegrees();
+
         addRequirements(m_drivetrain);
     }
 
@@ -27,6 +37,23 @@ public class Rotate extends CommandBase {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
+        double percentOutput = 0.0d;
+        m_error = Math.abs(m_drivetrain.getGyroYawDegrees() - Math.abs(initalAngle - m_targetDegrees));
+        
+        m_change = m_error - m_lastError;
+        
+        if(Math.abs(m_errorIntegral) < 50) // this is an integral limit to keep from excessive I commanded movement 
+        {
+            //Accumulate the error into the integral
+            m_errorIntegral += m_error;
+        }
+        
+        percentOutput = (m_vP * m_error) + (m_vI * m_errorIntegral) + (m_vD * m_change);
+        clampPercentOutput(percentOutput);
+
+        m_lastError = m_error;
+
+        m_drivetrain.percentDrive(-percentOutput, percentOutput);
     }
 
     // Called once the command ends or is interrupted.
@@ -38,12 +65,25 @@ public class Rotate extends CommandBase {
     @Override
     public boolean isFinished() {
         boolean retVal = false;
-        if(Math.abs(m_drivetrain.getGyroYawDegrees() - Math.abs(initalAngle - m_targetDegrees)) < 40){
+        if(m_error < 10){
             SmartDashboard.putBoolean("finished rotating", true);
             retVal = true;
         }
         
 
         return retVal;
+    }
+    
+    public double clampPercentOutput(double percent){
+        if(percent > 0.5) 
+        {
+            percent = 0.5;
+        }
+        else if(percent < -0.5) 
+        {
+            percent = -0.5;
+        }
+
+        return percent;
     }
 }
